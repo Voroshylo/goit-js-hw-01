4b312c808f91ac1ac1e478ec8651ba2201735777
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-03-10 17:24:16 +0200

    fix function task 3

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,8 @@ function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
+  const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+  return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
